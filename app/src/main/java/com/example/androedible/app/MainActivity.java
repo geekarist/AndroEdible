8c3805934b478d69b97cf1010d28d265e40ae978
chrp <christophe.pele@gmail.com>
2014-04-26 22:28:54 +0200

    Add jongo dependency

--- app/src/main/java/com/example/androedible/app/MainActivity.java
+++ app/src/main/java/com/example/androedible/app/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.androedible.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
